ba6f9207f7ab624e1f193fdeeeb28f39307f4e41
Pranav Vedagiri <pranav.vedagiri@gmail.com>
2020-09-24 00:02:41 -0700

    updated website and updated to latest version of puppeteer
    swag

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('https://bdgastore.com/blogs/upcoming-releases/9-23-20-nike-dunk-hi-sp-maize-blue');
-
+  setTimeout(function() {
+    console.log('hello world!');
+  }, 5000);
+  await page.goto('http://www.baitme.com/nike-kobe-v-protro-pj-tucker-pe');
+  setTimeout(function() {
+    console.log('hello world!');
+  }, 5000);
 //   await page.type('#first_name', 'Justin');
 //   await page.type('#last_name', 'Justin');
 
